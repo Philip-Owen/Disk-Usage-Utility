3d0ff769e8fef68f1dc7db17035bd0963d949356
philip-owen <owen.philipb@gmail.com>
2021-05-22 14:34:57 -0500

    added error handling logic for cases when  a mountpoint isnt specified or found.

--- getdiskusage.py
+++ getdiskusage.py
@@ -1,24 +1,32 @@
 import os, sys, json
 from pathlib import Path
 
-
-mount_point = sys.argv[1]
+try:
+    mount_point = sys.argv[1]
+except IndexError:
+    print(json.dumps({"error": "No mountpoint specified."}, indent=4))
+    exit()
 
 disk_usage = {"files": []}
 
 
 def find_file_sizes(mount_point):
-    contents = os.listdir(mount_point)
+    try:
+        contents = os.listdir(mount_point)
 
-    for file in contents:
-        path = os.path.join(mount_point, file)
+        for file in contents:
+            path = os.path.join(mount_point, file)
 
-        if not os.path.isdir(path):
-            disk_usage["files"].append(
-                {"path": path, "size": Path(path).stat().st_size}
-            )
-        else:
-            find_file_sizes(path)
+            if not os.path.isdir(path):
+                disk_usage["files"].append(
+                    {"path": path, "size": Path(path).stat().st_size}
+                )
+            else:
+                find_file_sizes(path)
+
+    except FileNotFoundError:
+        print(json.dumps({"error": f"No mountpoint '{mount_point}' found."}, indent=4))
+        exit()
 
 
 find_file_sizes(mount_point)
