7179e3d0d444c660534e9e9e56401094dd1d742f
philip-owen <owen.philipb@gmail.com>
2021-05-22 13:46:49 -0500

    initial commit. created recursive function to walk ddirectory paths and return all files within the scope of the mountpoint

---
+++ getdiskusage.py
@@ -0,0 +1,28 @@
+import os, sys, json
+from pathlib import Path
+
+
+mount_point = sys.argv[1]
+
+output = os.listdir(mount_point)
+
+disk_usage = {"files": []}
+
+
+def find_file_sizes(mount_point):
+    contents = os.listdir(mount_point)
+
+    for file in contents:
+        path = os.path.join(mount_point, file)
+
+        if not os.path.isdir(path):
+            disk_usage["files"].append(
+                {"path": path, "size": Path(path).stat().st_size}
+            )
+        else:
+            find_file_sizes(path)
+
+
+find_file_sizes(mount_point)
+
+print(json.dumps(disk_usage, indent=4))
